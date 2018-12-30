0926afacaf5160e712b1b477d39b8ec307bffdb9
huongnhdh <huong.nhdh@gmail.com>
2018-12-31 02:47:39 +0700

    fix url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://huongnhdh.github.io/wiki/'
+SITEURL = 'https://huongnhdh.github.io/blog/'
 
 RELATIVE_URLS = False
 
