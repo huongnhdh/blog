5448ea8a4342b11de98ba959c0255ce523061a16
huongnhd <huongnhd@lifull-tech.vn>
2019-04-18 13:59:43 +0700

    edit config

--- publishconf.py
+++ publishconf.py
@@ -26,7 +26,7 @@ DELETE_OUTPUT_DIRECTORY = False
 #DISQUS_SITENAME = ""
 GOOGLE_ANALYTICS = "UA-131511951-1"
 
-STATIC_PATHS = ['extra/favicon.ico']
+STATIC_PATHS = ['extra', 'images']
 EXTRA_PATH_METADATA = {
     'extra/favicon.ico': {'path': SITEURL + 'favicon.ico'}
 }
